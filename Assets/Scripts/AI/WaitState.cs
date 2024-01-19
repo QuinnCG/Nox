94683b208562307c1d70483ef9786010c77b4328
Quinton <86534924+QuinnCG@users.noreply.github.com>
2024-01-19 14:55:55 -0500

    Added "WaitState" to State.cs

---
+++ Assets/Scripts/AI/WaitState.cs
@@ -0,0 +1,22 @@
+﻿namespace Game.AI
+{
+	public class WaitState : State
+	{
+		public bool IsFinished { get; private set; }
+
+		private readonly float _duration;
+
+		public WaitState(float duration)
+		{
+			_duration = duration;
+		}
+
+		public override void OnStart(EnemyBrain agent)
+		{
+			IsFinished = false;
+
+			var timer = new Timer(_duration);
+			timer.OnFinish += () => IsFinished = true;
+		}
+	}
+}
