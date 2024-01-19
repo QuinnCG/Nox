3b2867e7d3377e2bcb726430e261675990e46047
Quinton <86534924+QuinnCG@users.noreply.github.com>
2024-01-19 13:53:31 -0500

    #1 Sub System State
    A state that can run sub state-machines.

---
+++ Assets/Scripts/AI/States/SubSystemState.cs
@@ -0,0 +1,27 @@
+﻿namespace Game.AI.States
+{
+	public class SubSystemState : State
+	{
+		private readonly StateMachine _stateMachine;
+
+		public SubSystemState(StateMachine subSystem)
+		{
+			_stateMachine = subSystem;
+		}
+
+		public override void OnStart(EnemyBrain agent)
+		{
+			_stateMachine.Start();
+		}
+
+		public override void OnUpdate(EnemyBrain agent)
+		{
+			_stateMachine.Update();
+		}
+
+		public override void OnFinish(EnemyBrain agent)
+		{
+			_stateMachine.Finish();
+		}
+	}
+}
