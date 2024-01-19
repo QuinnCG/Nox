31c53e7045a430be3b64baea9538870a11f1a2a0
Quinton <86534924+QuinnCG@users.noreply.github.com>
2024-01-18 23:32:38 -0500

    Added a Robust, General Purpose Timer
    - Very handy for all timer related needs
    - Performant: it calculates your remaining time as you poll it not every frame
    - Callback: you can subscribe to a callback method to be notified when the time is over

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,42 @@
+﻿using DG.Tweening;
+using System;
+using UnityEngine;
+
+namespace Game
+{
+	public class Timer
+	{
+		public float Start { get; private set; }
+		public float End => Start + Duration;
+		public float Duration { get; private set; }
+		public float Elapsed => Time.time - Start;
+		public float Remaining => End - Time.time;
+
+		public event Action OnFinish;
+
+		private Tween _tween;
+
+		public Timer(float duration)
+		{
+			Duration = duration;
+			Reset();
+		}
+
+		public static Action Wait(float duration)
+		{
+			var timer = new Timer(duration);
+			return timer.OnFinish;
+		}
+
+		public void Reset()
+		{
+			Start = Time.time;
+
+			_tween?.Kill();
+			_tween = DOVirtual.DelayedCall(Duration, () =>
+			{
+				OnFinish?.Invoke();
+			}, ignoreTimeScale: false);
+		}
+	}
+}
