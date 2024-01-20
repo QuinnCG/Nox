faa1168f5a248b632ac50952a96e92171190f351
Quinton <86534924+QuinnCG@users.noreply.github.com>
2024-01-20 00:33:18 -0500

    Tweaks

--- Assets/Scripts/UI/SubtleHeartbeats.cs
+++ Assets/Scripts/UI/SubtleHeartbeats.cs
@@ -4,7 +4,6 @@ using DG.Tweening;
 
 public class SubtleHeartbeats : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-
     public AnimationCurve curve;
     public float minScale = 0.06f;
     public float maxScale = 0.09f;
@@ -19,13 +18,11 @@ public class SubtleHeartbeats : MonoBehaviour, IPointerEnterHandler, IPointerExi
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-
         StartSubtlePulsating();
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-
         StopPulsating();
     }
 
@@ -33,14 +30,12 @@ public class SubtleHeartbeats : MonoBehaviour, IPointerEnterHandler, IPointerExi
     {
         transform.DOScale(maxScale, pulseDuration)
             .SetEase(curve)
-            .SetLoops(-1, LoopType.Restart)
-            .SetId("SubtlePulsatingTween");
+            .SetLoops(-1);
     }
 
     void StopPulsating()
     {
-
-        DOTween.Kill("SubtlePulsatingTween");
+		transform.DOKill();
         transform.DOScale(originalScale, 0.2f);
     }
 }
