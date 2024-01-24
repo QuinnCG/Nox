f8b40e6fbd910884e3b4f2b9c0907aff430ba273
Quinton <86534924+QuinnCG@users.noreply.github.com>
2024-01-24 16:19:46 -0500

    Added "Parallel" Composite Node
    Used to run sub nodes in a tree in tandem to one another.

---
+++ Assets/Scripts/AI/BehaviorTree/Composites/Parallel.cs
@@ -0,0 +1,25 @@
+﻿namespace Game.AI.BehaviorTree.Composites
+{
+	public class Parallel : BTComposite
+	{
+		protected override BTStatus OnUpdate()
+		{
+			bool running = false;
+
+			foreach (var child in Children)
+			{
+				BTStatus status = child.Update();
+				if (status == BTStatus.Failure)
+				{
+					return BTStatus.Failure;
+				}
+				else if (status == BTStatus.Running)
+				{
+					running = true;
+				}
+			}
+
+			return running ? BTStatus.Running : BTStatus.Success;
+		}
+	}
+}
