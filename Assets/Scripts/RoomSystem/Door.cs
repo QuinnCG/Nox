d2e39afcecdcc7eeb97a85936963926bc6f19f0b
Quinton <86534924+QuinnCG@users.noreply.github.com>
2024-01-29 19:53:43 -0500

    Added door art

--- Assets/Scripts/RoomSystem/Door.cs
+++ Assets/Scripts/RoomSystem/Door.cs
@@ -1,17 +1,66 @@
+using FMODUnity;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace Game.RoomSystem
 {
+	[RequireComponent(typeof(SpriteRenderer))]
+	[RequireComponent(typeof(Collider2D))]
 	public class Door : MonoBehaviour
 	{
-		public void Open()
+		[SerializeField, Required]
+		private Sprite OpenSprite, ClosedSprite;
+
+		[SerializeField]
+		private EventReference OpenSound, CloseSound;
+
+		[SerializeField]
+		private bool StartOpen;
+
+		public bool IsOpen { get; private set; }
+
+		private SpriteRenderer _renderer;
+		private Collider2D _collider;
+
+		private void Awake()
 		{
-			gameObject.SetActive(false);
+			_renderer = GetComponent<SpriteRenderer>();
+			_collider = GetComponent<Collider2D>();
+
+			if (StartOpen)
+			{
+				Open(true);
+			}
 		}
 
-		public void Close()
+		public void Open(bool supressFX = false)
 		{
-			gameObject.SetActive(true);
+			if (!IsOpen)
+			{
+				IsOpen = true;
+				_renderer.sprite = OpenSprite;
+				_collider.enabled = false;
+
+				if (!supressFX && !OpenSound.IsNull)
+				{
+					RuntimeManager.PlayOneShot(OpenSound, transform.position);
+				}
+			}
+		}
+
+		public void Close(bool supressFX = false)
+		{
+			if (!IsOpen)
+			{
+				IsOpen = false;
+				_renderer.sprite = ClosedSprite;
+				_collider.enabled = true;
+
+				if (!supressFX && !CloseSound.IsNull)
+				{
+					RuntimeManager.PlayOneShot(CloseSound, transform.position);
+				}
+			}
 		}
 	}
 }
