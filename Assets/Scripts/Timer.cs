b3c93336e09648e66a56730b49f4fb6cadb774ba
Quinton <86534924+QuinnCG@users.noreply.github.com>
2024-01-29 23:07:31 -0500

    Added option to start timer already finished

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -7,19 +7,22 @@ namespace Game
 	public class Timer
 	{
 		public float Start { get; private set; }
-		public float End => Start + Duration;
+		public float End { get; private set; }
 		public float Duration { get; private set; }
 		public float Elapsed => Time.time - Start;
 		public float Remaining => End - Time.time;
 		public bool IsDone => Remaining <= 0f;
 
+		public bool StartFinished { get; set; } = false;
+
 		public event Action OnFinish;
 
 		private Tween _tween;
 
-		public Timer(float duration)
+		public Timer(float duration, bool startFinished = false)
 		{
 			Duration = duration;
+			StartFinished = startFinished;
 			Reset();
 		}
 
@@ -32,6 +35,7 @@ namespace Game
 		public void Reset()
 		{
 			Start = Time.time;
+			End = StartFinished ? Start : Start + Duration;
 
 			_tween?.Kill();
 			_tween = DOVirtual.DelayedCall(Duration, () =>
