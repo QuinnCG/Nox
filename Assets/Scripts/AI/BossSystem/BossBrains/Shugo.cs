cebb93c3dbd961e78dd7590a36b16354e977d1a4
Quinton <86534924+QuinnCG@users.noreply.github.com>
2024-02-03 16:24:18 -0500

    Saniziation

--- Assets/Scripts/AI/BossSystem/BossBrains/Shugo.cs
+++ Assets/Scripts/AI/BossSystem/BossBrains/Shugo.cs
@@ -267,11 +267,11 @@ namespace Game.AI.BossSystem.BossBrains
 			yield return jump.Yield();
 
 			AudioManager.PlayOneShot(FireSpewStartSound);
-      Debug.Log("Before FireSpewStart Animation");
-      Animator.Play(FireSpewStart).Yield();
-      Debug.Log("After FireSpewStart Animation");
+			Debug.Log("Before FireSpewStart Animation");
+			Animator.Play(FireSpewStart).Yield();
+			Debug.Log("After FireSpewStart Animation");
 
-			
+
 
 			for (int i = 0; i < FireSpewWaveCount; i++)
 			{
