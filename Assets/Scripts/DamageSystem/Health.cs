15c89385d1058a0003fca2630a2daf31714cb1a8
Quinton <86534924+QuinnCG@users.noreply.github.com>
2024-02-03 22:14:05 -0500

    Small fixes

--- Assets/Scripts/DamageSystem/Health.cs
+++ Assets/Scripts/DamageSystem/Health.cs
@@ -1,9 +1,11 @@
 ﻿using DG.Tweening;
+using FMOD.Studio;
 using FMODUnity;
 using Game.GeneralManagers;
 using Game.Player;
 using Sirenix.OdinInspector;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
