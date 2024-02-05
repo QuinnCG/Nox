b9f5b332d25684200dc0fc1fb0c35a0987e26d47
Quinton <86534924+QuinnCG@users.noreply.github.com>
2024-02-05 06:17:42 -0500

    Cleaned up unused code

--- Assets/Scripts/UI/HUD.cs
+++ Assets/Scripts/UI/HUD.cs
@@ -14,18 +14,6 @@ namespace Game.UI
 	{
 		public static HUD Instance { get; private set; }
 
-		//[SerializeField, BoxGroup("Animation Settings")]
-		//private float PlayerHealthFadeIn = 1f;
-
-		//[SerializeField, BoxGroup("Animation Settings")]
-		//private float PlayerHealthFadeOut = 2f;
-
-		//[SerializeField, BoxGroup("Animation Settings")]
-		//private float PlayerHealthCriticalPulseDuration = 0.5f;
-
-		//[SerializeField, BoxGroup("Animation Settings")]
-		//private float PlayerHealthCriticalPulseScale = 1.2f;
-
 		[SerializeField, BoxGroup("References"), Required]
 		private Slider PlayerHealth;
 
@@ -50,7 +38,6 @@ namespace Game.UI
 		[field: SerializeField, BoxGroup("References"), Required]
 		public GameObject GameOverRoot { get; private set; }
 
-		private Character _lastCharacter;
 		private Health _health;
 
 		private void Awake()
@@ -63,7 +50,6 @@ namespace Game.UI
 			PossessionManager.Instance.OnCharacterPossessed += OnCharacterPossessed;
 			OnCharacterPossessed(PossessionManager.Instance.PossessedCharacter);
 
-			HidePlayerHealth();
 			HideBoss();
 		}
 
@@ -138,97 +124,6 @@ namespace Game.UI
 				// Handle the case when the character is null.
 				return;
 			}
-
-			// Clean up old character.
-			if (_lastCharacter != null)
-			{
-				var lastHealth = _lastCharacter.GetComponent<Health>();
-				if (lastHealth != null)
-				{
-					lastHealth.OnDamaged -= OnDamaged;
-					lastHealth.OnReachMaxHealth -= FadeOutPlayerHealth;
-				}
-			}
-
-			// Update new character.
-			if (character.TryGetComponent(out _health))
-			{
-				_health.OnDamaged += OnDamaged;
-				_health.OnReachMaxHealth += FadeOutPlayerHealth;
-
-				if (_health.IsCritical)
-				{
-					FadeInPlayerHealth();
-				}
-				else
-				{
-					FadeOutPlayerHealth();
-				}
-			}
-
-			// Update _lastCharacter for use in next possession.
-			_lastCharacter = character;
-		}
-
-		private void OnDamaged(float damage)
-		{
-			FadeInPlayerHealth();
-		}
-
-		private void FadeInPlayerHealth()
-		{
-			// This is not really needed as you never wander around and pretty much always need to see your heatlh bar.
-
-			//var renderers = PlayerHealth.GetComponentsInChildren<Image>();
-
-			//renderers[0].DOKill();
-			//renderers[1].DOKill();
-
-			//renderers[0].DOFade(1f, PlayerHealthFadeIn).SetEase(Ease.Linear);
-			//renderers[1].DOFade(1f, PlayerHealthFadeIn).SetEase(Ease.Linear);
-
-			//// Optional: Pulse animation for critical health
-			//if (_health.IsCritical)
-			//{
-			//	var handleRectTransform = PlayerHealth.GetComponentInChildren<RectTransform>();
-
-			//	handleRectTransform.DOScale(PlayerHealthCriticalPulseScale, PlayerHealthCriticalPulseDuration / 2)
-			//			.SetEase(Ease.InOutSine)
-			//			.OnComplete(() =>
-			//			{
-			//				handleRectTransform.DOScale(1f, PlayerHealthCriticalPulseDuration / 2)
-			//				.SetEase(Ease.InOutSine);
-			//			});
-			//}
-
-		}
-
-		private void FadeOutPlayerHealth()
-		{
-			// This is not really needed as you never wander around and pretty much always need to see your heatlh bar.
-
-			//var renderers = PlayerHealth.GetComponentsInChildren<Image>();
-
-			//renderers[0].DOKill();
-			//renderers[1].DOKill();
-
-			//renderers[0].DOFade(0f, PlayerHealthFadeOut).SetEase(Ease.Linear);
-			//renderers[1].DOFade(0f, PlayerHealthFadeOut).SetEase(Ease.Linear);
-		}
-
-		private void HidePlayerHealth()
-		{
-			// This is not really needed as you never wander around and pretty much always need to see your heatlh bar.
-
-			//var renderers = PlayerHealth.GetComponentsInChildren<Image>();
-
-			//var color = renderers[0].color;
-			//color.a = 0f;
-			//renderers[0].color = color;
-
-			//color = renderers[1].color;
-			//color.a = 0f;
-			//renderers[1].color = color;
 		}
 
 		private void ShowBoss()
